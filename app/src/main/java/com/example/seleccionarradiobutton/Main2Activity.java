ba9daf3d81edb922762464ba7eb940e259bdd3c7
pedrojesus07 <pedrojesus_95_baza@hotmail.com>
2019-10-29 11:12:54 +0100

    1

--- app/src/main/java/com/example/seleccionarradiobutton/Main2Activity.java
+++ app/src/main/java/com/example/seleccionarradiobutton/Main2Activity.java
@@ -26,7 +26,9 @@ public class Main2Activity extends AppCompatActivity {
     private String opcion;
     private Spinner spn;
     private ArrayAdapter<String> dataAdapter;
+    private ArrayAdapter<String> dataAdapter2;
     private Button btAcumular;
+    private float acumulado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +39,7 @@ public class Main2Activity extends AppCompatActivity {
 
         initComponents();
         additem();
-        listenerSpinner();
+        //listenerSpinner();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -51,29 +53,39 @@ public class Main2Activity extends AppCompatActivity {
         Log.v("xvy",opcion);
     }
 
-    private void listenerSpinner() {
-        btAcumular.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.v("cvv",spn.getSelectedItem().toString());
-            }
-        });
-    }
-
     private void initComponents() {
         Intent intent = new Intent();
         intent = getIntent();
         opcion = intent.getStringExtra("opcion");
 
         spn = findViewById(R.id.spn);
+        btAcumular = findViewById(R.id.btAcumular);
     }
+
+    private void listenerSpinner() {
+        btAcumular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("cvv",spn.getSelectedItem().toString());
+                Log.v("cvv1",spn.getSelectedItemPosition() + "");
+                acumulado =
+            }
+        });
+    }
+
 
     private void additem() {
         Resources res = getResources();
+
         String[] lacteos = res.getStringArray(R.array.Lacteos);
         String[] carne = res.getStringArray(R.array.Carne);
         String[] pescado = res.getStringArray(R.array.Pescado);
         String[] verdura = res.getStringArray(R.array.Verdura);
+
+        String[] lacteosPrecio = res.getStringArray(R.array.LacteosPrecio);
+        String[] carnePrecio = res.getStringArray(R.array.CarnePrecio);
+        String[] pescadoPrecio = res.getStringArray(R.array.PescadoPrecio);
+        String[] verduraPrecio = res.getStringArray(R.array.VerduraPrecio);
 
         switch (opcion){
             case "1":
@@ -81,24 +93,28 @@ public class Main2Activity extends AppCompatActivity {
                         android.R.layout.simple_spinner_item, lacteos);
                 dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spn.setAdapter(dataAdapter);
+                listenerSpinner();
                 break;
             case "2":
                 dataAdapter = new ArrayAdapter<String>(this,
                         android.R.layout.simple_spinner_item, carne);
                 dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spn.setAdapter(dataAdapter);
+                listenerSpinner();
                 break;
             case "3":
                 dataAdapter = new ArrayAdapter<String>(this,
                         android.R.layout.simple_spinner_item, pescado);
                 dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spn.setAdapter(dataAdapter);
+                listenerSpinner();
                 break;
             case "4":
                 dataAdapter = new ArrayAdapter<String>(this,
                         android.R.layout.simple_spinner_item, verdura);
                 dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spn.setAdapter(dataAdapter);
+                listenerSpinner();
                 break;
 
         }
